9a222be2ff9563642432f6e0831e355c34a54e8d
possiblemeatball <mwalker@possiblemeatball.net>
2022-04-11 20:11:34 -0400

    starting out and .gitignore additions

---
+++ src/main/java/net/manthrowshat/shmurdabot/main/BotJavaMain.java
@@ -0,0 +1,13 @@
+package net.manthrowshat.shmurdabot.main;
+
+import java.util.Arrays;
+
+public class BotJavaMain {
+    /**
+     * public static void main
+     * @param args (String...)
+     */
+    public static void main(String... args) {
+        System.out.println("hello " + Arrays.toString(args));
+    }
+}
